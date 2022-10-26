9ff4c77cffa82264a9f573eb55ace78f889f4eb3
Vince Pelss <vpelss@users.noreply.github.com>
2022-10-25 20:40:57 -0400

    1

--- inc/menu.php
+++ inc/menu.php
@@ -1,21 +1,18 @@
 <?php
 
-global $imok_menu;
-$imok_menu = 'imok_menu';
+global $imok_menu_name;
+$imok_menu_name = 'imok_menu';
 
-add_action('init', 'auto_nav_creation_primary');
-function auto_nav_creation_primary(){
-
-global $imok_menu;
-
-$menu_exists = wp_get_nav_menu_object( $imok_menu );
-
+add_action('init', 'imok_nav_creation_primary');
+function imok_nav_creation_primary(){
+global $imok_menu_name;
+$menu_exists = wp_get_nav_menu_object( $imok_menu_name );
 if( !$menu_exists){ // If it doesn't exist, let's create it.
-    $menu_id = wp_create_nav_menu( $imok_menu );
+    $menu_id = wp_create_nav_menu( $imok_menu_name );
 
 	$dir = IMOK_PLUGIN_PATH . "/pages/";
 	$files = scandir($dir);
-	$menu_names = array();
+
 	foreach ($files as $file) {
 		if( ! str_starts_with( $file, 'IMOK' ) ){continue;}
 		$page = get_page_by_title($file);
@@ -27,24 +24,21 @@ if( !$menu_exists){ // If it doesn't exist, let's create it.
         'menu-item-classes' => '',
         'menu-item-url' => $guid,
         'menu-item-status' => 'publish'));
-	}
-
-}
-
+    	}
+    }
 };
 
 add_action('shutdown', 'shutdown_funct');
-function shutdown_funct(){
-	global $imok_menu;
-	wp_delete_nav_menu( $imok_menu );
+function shutdown_funct(){ //maybe move to deactivate
+	global $imok_menu_name;
+	wp_delete_nav_menu( $imok_menu_name );
 };
-
 
 add_shortcode( 'imok_menu', 'imok_menu_func' );
 function imok_menu_func(){
-	global $imok_menu;
+	global $imok_menu_name;
 
-	$txt = wp_nav_menu( array( 'menu'=> $imok_menu ,
+	$page_links = wp_nav_menu( array( 'menu'=> $imok_menu_name ,
 									'menu_id' => 'imok_menu_id',
 									'menu_class' => "menu",
 									'echo'=>false ,
@@ -54,30 +48,92 @@ function imok_menu_func(){
 									//'items_wrap' => '%3$s'
 								  ) );
 
-	return $txt;
+    $frills = "
+    <!--
+https://www.woolston.com.au/so-simple-collapsible-responsive-navigation-using-only-css/
+https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp
+-->
 
+<style>
+.overlay {
+  max-width: 2000px;
+  height: 100%;
+  width: 100%;
+  display: none;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: white;
+  _background-color: rgb(0,0,0);
+  _background-color: rgba(0,0,0, 0.9);
+}
+
+#imok_menu_id{
+    list-style-type: none;
+}
+
+.overlay-content , #imok_menu{
+  position: relative;
+  top: 20%;
+  //25%
+  width: 100%;
+  text-align: center;
+  margin-top: 30px;
+}
+
+.overlay a {
+  padding: 8px;
+  text-decoration: none;
+  font-size: 30px;
+  //36px;
+  _color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.overlay a:hover, .overlay a:focus {
+  color: #f1f1f1;
+}
+
+.overlay .closebtn {
+  position: absolute;
+  top: 20px;
+  right: 45px;
+  font-size: 60px;
+}
+
+@media screen and (max-height: 550px) {
+  .overlay a {font-size: 20px}
+  .overlay .closebtn {
+  font-size: 40px;
+  top: 15px;
+  right: 35px;
+  }
+}
+</style>
+
+<center>
+<span style='font-size:30px;cursor:pointer;' onclick='openNav()'>☰</span>
+</center>
+
+<div id='myNav' class='overlay'>
+<a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>×</a>
+{$page_links}
+</div>
+
+<script>
+function openNav() {
+  document.getElementById('myNav').style.display = 'block';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.display = 'none';
+}
+</script>
+";
+
+	return $frills;
 	}
-
-	/*
-	$rrr = wp_nav_menu( array(
-	'menu'				=> 'imok_menu', // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
-	'menu_class'		=> "", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
-	'menu_id'			=> "", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
-	'container'			=> "", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
-	'container_class'	=> "", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
-	'container_id'		=> "", // (string) The ID that is applied to the container.
-	'fallback_cb'		=> "", // (callable|bool) If the menu doesn't exists, a callback function will fire. Default is 'wp_page_menu'. Set to false for no fallback.
-	'before'			=> "", // (string) Text before the link markup.
-	'after'				=> "", // (string) Text after the link markup.
-	'link_before'		=> "", // (string) Text before the link text.
-	'link_after'		=> "", // (string) Text after the link text.
-	'echo'				=> "", // (bool) Whether to echo the menu or return it. Default true.
-	'depth'				=> "", // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
-	'walker'			=> "", // (object) Instance of a custom walker class.
-	'theme_location'	=> "", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
-	'items_wrap'		=> "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
-	'item_spacing'		=> "", // (string) Whether to preserve whitespace within the menu's HTML. Accepts 'preserve' or 'discard'. Default 'preserve'.
-) );
-*/
 
 ?>
